5448f0f2e221aa8d76c6ddd77330f633b116a867
Vador <anass.den@gmail.com>
2018-03-20 10:34:44 +0100

    hrrc freq domain

--- main.m
+++ main.m
@@ -1,6 +1,7 @@
 addpath(genpath('Code encodeur'));
 addpath(genpath('Code mapping-demapping'));
 
+clear all; close all;
 % Generate a stream tx of random bits
 N = 1000; % length of the stream
 fs = 10^6;
@@ -9,10 +10,10 @@ modulation = 'qam'; % or pam
 bit_tx = randi(2,N,1)-1;
 symb_tx = mapping(bit_tx,Nbps,modulation);
 
-Fsampling = 8e8;
+Fsampling = 8e5;
 beta = 0.5;
 Tsymb = 1e-6;
 
-f = 0:Fsampling/2;
+f = -Fsampling/2:1000:Fsampling/2;
 HRRC = HRRC(f,Tsymb,beta);
 plot(f,HRRC)
