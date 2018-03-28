024586b4caf9985641c0d67bf068975a77908c41
Trong Quy Phan <p.trongquy@gmail.com>
2018-03-28 12:13:12 +0200

    ok

--- project_modulation.m
+++ project_modulation.m
@@ -1,4 +1,4 @@
-% Projet modulation & coding
+%% Projet modulation & coding
 addpath(genpath('Code encodeur'));
 addpath(genpath('Code mapping-demapping'));
 clear; close all;
@@ -24,21 +24,26 @@ signal = mapping(bits_tx,Nbps,modulation);
 signal_tx = upsample(signal,M);
 
 %% implementation of transfer function
-RRCtaps = 365;
+RRCtaps = 65;
 stepoffset = (1/RRCtaps)*fsampling;
 highestfreq = (RRCtaps-1)*stepoffset/2;
 f = linspace(-highestfreq,highestfreq,RRCtaps);
 Hrrc = HRRC(f,Tsymb,beta);
-h_freq = (fft(ifft(Hrrc)/max(ifft(Hrrc))));
-% figure;
-% plot(f,abs((Hrrc)/max((Hrrc))));
+h_t = ifft(Hrrc);
+h_freq = sqrt(fft(h_t/max(h_t)));
 
-h_time = fftshift(ifft(fftshift(h_freq)));
+% h_freq = sqrt(fft(ifft(Hrrc)/max(ifft(Hrrc))));
+% h_freq = (fft(ifft(Hrrc)/max(ifft(Hrrc))));
+
+% figure;
+% plot(f,h_freq);
+
+h_time = fftshift(ifft(ifftshift(h_freq)));
 deltat = 1/fsampling;
 t = (-(RRCtaps-1)/2:(RRCtaps-1)/2)*deltat;
 
-figure;
-plot(t,abs(h_time));
+% figure;
+% plot(t,h_time);
 % hold on
 % plot(t+Tsymb,h_time);
 % hold on
@@ -47,31 +52,32 @@ plot(t,abs(h_time));
 % plot(t+3*Tsymb,h_time);
 % hold on
 % plot(t+4*Tsymb,h_time);
-grid on;
+% grid on;
+
 %% Convolution
 signal_hrrc_tx = conv(signal_tx, h_time);
 % figure;
 % stem(signal_hrrc_tx);
+
 %% Noise through the channel
 signal_power = (trapz(abs(signal_hrrc_tx)).^2)*(1/fsampling); % total power
 Eb = signal_power/N; % energy per bit
-EbN0 = 1; % SNR (parameter)
+EbN0 = 50; % SNR (parameter)
 N0 = Eb/EbN0; 
 NoisePower = 2*N0*fsampling;
 noise = sqrt(NoisePower/2)*(randn(length(signal_hrrc_tx),1)+1i*randn(length(signal_hrrc_tx),1));
 
-signal_rx = signal_hrrc_tx ;%+ noise;
+signal_rx = signal_hrrc_tx; %+ noise;
 signal_hhrc_rx = conv(signal_rx, h_time);
 
 signal_hhrc_rx_trunc = signal_hhrc_rx(RRCtaps:end-RRCtaps+1);
 
 %% downsampling
 signal_rx_down = downsample(signal_hhrc_rx_trunc, M);
+
 %% demapping
 bits_rx = demapping(signal_rx_down,Nbps,modulation);
 
 if bits_rx == bits_tx
     disp('ok')
 end
-
-
