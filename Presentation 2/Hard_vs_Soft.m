c80c8ea9fc716c3e499a1f51f242dea431b31b93
Trong Quy Phan <p.trongquy@gmail.com>
2018-05-02 13:20:25 +0200

    Code graphes

---
+++ Presentation 2/Hard_vs_Soft.m
@@ -0,0 +1,8 @@
+load BER_iterHardBPSK.mat; load EB_N0_iterHardBPSK.mat
+load BER_iterSoftBPSK.mat; load EB_N0_iterSoftBPSK.mat
+semilogy(EbN0_hard,BER_hard(:,1),'-',EbN0_hard,BER_hard(:,2),'-',EbN0_soft,BER_soft(:,3),'-');
+xlabel('E_B/N_0 [dB]');
+ylabel('BER');
+legend('Uncoded','Hard','Soft');
+title('Hard vs Soft decoding for BPSK with 4 iterations')
+grid on;
